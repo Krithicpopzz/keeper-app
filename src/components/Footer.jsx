ab6927c2bbee64ccdc566116837456b99b0eee9f
Krithicsugumar <63303515+Krithicpopzz@users.noreply.github.com>
2025-09-09 19:25:58 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ Krithic</p>
+    </footer>
+  );
+}
+
+export default Footer;
